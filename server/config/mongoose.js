fe673435cf384cd6c4633709bca34d21d1006e76
ParasaraIB <manikparasara@ymail.com>
2022-10-20 18:14:11 +0800

    fix db connection error through uri

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,12 +1,13 @@
 "use strict";
 
 const mongoose = require("mongoose");
+const {ServerApiVersion} = require("mongodb")
 
-const uri = process.env.DB_URL;
+const uri = process.env.DB_URL2;
 
 async function mongooseConnect() {
   try {
-    await mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+    await mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true, serverApi:ServerApiVersion.v1});
     console.log("Successfully connected to MongoDB");
   } catch (err) {
     console.error(err, "<<<< Failed connecting to MongoDB");
